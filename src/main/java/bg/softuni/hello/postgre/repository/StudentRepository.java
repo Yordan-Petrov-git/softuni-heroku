e368957dc2627f3181386dac0284f236e2405950
Lachezar Balev <lachezar.balev@rewe-digital.com>
2020-08-06 18:16:30 +0300

    New attempt

--- src/main/java/bg/softuni/hello/postgre/repository/StudentRepository.java
+++ src/main/java/bg/softuni/hello/postgre/repository/StudentRepository.java
@@ -1,10 +1,10 @@
-//package bg.softuni.hello.postgre.repository;
-//
-//import bg.softuni.hello.postgre.model.Student;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface StudentRepository extends JpaRepository<Student, Long> {
-//
-//}
+package bg.softuni.hello.postgre.repository;
+
+import bg.softuni.hello.postgre.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
